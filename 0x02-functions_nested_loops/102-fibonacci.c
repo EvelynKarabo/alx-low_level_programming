37c18ab632acdc9bba286b4255620342cd972dda
EvelynKarabo <karabelomoabelo90@gmail.com>
2023-03-21 06:13:42 -0700

    103-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,24 +8,23 @@
 int main(void)
 
 {
-
-	int i = 0;
-	long j = 1, k = 2;
-
-	while (i < 50)
+int i = 0;
+long j = 1, k = 2;
+       
+while (i < 50)
 	{
-	if (i == 0)
+if (i == 0)
 	printf("%ld", j);
-	else if (i == 1)
+else if (i == 1)
 	printf(", %ld", k);
-	else
+else
 	{
 	k += j;
 	j = k - j;
 	printf(", %ld", k);
-        }
+	}
 	++i
 	}
 	printf("\n");
-	return (0);
+return (0);
 }
